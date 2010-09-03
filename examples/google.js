a32f8f8c101ce5871366a283403bdd1b84dcf2ee
Tj Holowaychuk <tj@vision-media.ca>
2010-09-03 14:05:36 -0700

    Remove requirement of star prefixing browser names (wtf?)

--- examples/google.js
+++ examples/google.js
@@ -9,11 +9,14 @@ var browser = selenium.createClient({
   host: 'localhost',
   port: 4444,
   url: 'http://www.google.com',
-  browser: '*firefox'
+  browser: 'firefox'
 });
 
 browser.session(function(err){
   browser.command('open', ['/'], function(err, res){
     console.dir(res.body)
+    browser.command('testComplete', [], function(err, res){
+      console.dir(res.body)
+    });
   });
 });
