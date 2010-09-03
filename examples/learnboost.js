e31816822e64ebd8247da4c3a21fc7ae4a20f8bd
Tj Holowaychuk <tj@vision-media.ca>
2010-09-03 16:58:08 -0700

    Started lb example

---
+++ examples/learnboost.js
@@ -0,0 +1,35 @@
+
+/**
+ * Module dependencies.
+ */
+
+var soda = require('../index');
+
+var browser = soda.createClient({
+  host: 'localhost',
+  port: 4444,
+  url: 'http://sirrobertborden.ca.app.learnboost.com/',
+  browser: 'firefox'
+});
+
+browser.session(function(){
+  browser
+    .chain
+    .open('/')
+    .clickAndWait('css=#login-box')
+    .click('Add Class')
+    .type('classroom[name]', 'Me Iz In Your Webzite')
+    .type('classroom[description]', 'Controlling urz Browzer')
+    .click('classroom[startdate]')
+    .click('//tr[3]/td[4]/div')
+    .click('classroom[enddate]')
+    .click('//tr[4]/td[4]/div')
+    .click('//input[@value="Save"]')
+    .clickAndWait('link=Log out')
+    .testComplete()
+    .done(function(err, res){
+      if (err) throw err;
+      console.log('done');
+    });  
+});
+  
