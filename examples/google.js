11f2351c482a3beea06830d8f6721b2f067c194e
Tj Holowaychuk <tj@vision-media.ca>
2010-09-03 14:27:30 -0700

    Better exception message again

--- examples/google.js
+++ examples/google.js
@@ -15,14 +15,11 @@ var browser = selenium.createClient({
 browser.session(function(err){
   browser.command('open', ['/'], function(err, res){
     browser.command('type', ['q', 'Hello World'], function(err, res){
-      browser.command('click', ['btnG'], function(err, res){
-        browser.command('assertTitle', ['Google'], function(err, res){
+      browser.command('clickAndWait', ['btnG'], function(err, res){
+        browser.command('assertTitle', ['Hello World - Google Search'], function(err, res){
           if (err) throw err;
-          browser.command('assertTextPresent', ['Hello'], function(err, res){
-            if (err) throw err;
-            browser.command('testComplete', [], function(err, res){
-              console.dir(res.body)
-            });
+          browser.command('testComplete', [], function(err, res){
+            console.dir(res.body)
           });
         });
       });
