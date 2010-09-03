58c1bd4254adc879ee1eeb2c5fee38bc84617012
Tj Holowaychuk <tj@vision-media.ca>
2010-09-03 14:59:06 -0700

    Added more soda.createClient() tests

--- test/client.test.js
+++ test/client.test.js
@@ -8,10 +8,30 @@ var soda = require('soda');
 module.exports = {
   'test .createClient() defaults': function(assert){
     var client = soda.createClient({
-      browser: '*firefox',
       url: 'http://www.google.com'
     });
+    assert.equal('*firefox', client.browser);
     assert.equal('localhost', client.host);
     assert.equal(4444, client.port);
+  },
+  
+  'test .createClient() options': function(assert){
+    var client = soda.createClient({
+      browser: '*safari',
+      url: 'http://www.google.com',
+      host: 'foobar',
+      port: 1234
+    });
+    assert.equal('*safari', client.browser);
+    assert.equal('foobar', client.host);
+    assert.equal(1234, client.port);
+  },
+  
+  'test .createClient() browser option': function(assert){
+    var client = soda.createClient({
+      browser: 'firefox',
+      url: 'http://www.google.com'
+    });
+    assert.equal('*firefox', client.browser);
   }
 };
