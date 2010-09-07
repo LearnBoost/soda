6493bfab3b2bb7dc65e33008179429887141c02f
Tj Holowaychuk <tj@vision-media.ca>
2010-09-07 14:48:58 -0700

    s/path/url/

--- test/sauce.test.js
+++ test/sauce.test.js
@@ -6,8 +6,8 @@
 var soda = require('soda');
 
 module.exports = {
-  'test SauceClient.path()': function(assert){
-    var path = soda.SauceClient.path('tjholowaychuk', '123', 'video.flv');
+  'test SauceClient.url()': function(assert){
+    var path = soda.SauceClient.url('tjholowaychuk', '123', 'video.flv');
     assert.equal('https://saucelabs.com/rest/tjholowaychuk/jobs/123/results/video.flv', path);
   }
 };
