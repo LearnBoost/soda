cd50a138e16a19a5646b5ccd60dc327414a05451
Tj Holowaychuk <tj@vision-media.ca>
2010-09-07 15:17:01 -0700

    Added SauceClient#video()

--- test/sauce.test.js
+++ test/sauce.test.js
@@ -9,5 +9,12 @@ module.exports = {
   'test SauceClient.url()': function(assert){
     var path = soda.SauceClient.url('tjholowaychuk', '123', 'video.flv');
     assert.equal('https://saucelabs.com/rest/tjholowaychuk/jobs/123/results/video.flv', path);
+  },
+  
+  'test SauceClient.video()': function(assert){
+    var str = soda.SauceClient.video('tjholowaychuk', 'my-api-key', '123');
+    assert.equal(
+        '<script src="http://saucelabs.com/video-embed/123.js?username=tjholowaychuk&access_key=my-api-key"/>'
+      , str);
   }
 };
