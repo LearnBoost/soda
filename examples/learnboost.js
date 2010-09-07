d2c9ff8c29a00c849e919cb781037e574f4b6dc0
Tj Holowaychuk <tj@vision-media.ca>
2010-09-07 13:34:12 -0700

    Fixed SauceClient portno

--- examples/learnboost.js
+++ examples/learnboost.js
@@ -9,12 +9,16 @@ var browser = soda.createClient({
   url: 'http://sirrobertborden.ca.app.learnboost.com/'
 });
 
+browser.on('command', function(cmd, args){
+  console.log(' \x1b[33m%s\x1b[0m: %s', cmd, args.join(', '));
+});
+
 browser
   .chain
   .session()
   .setTimeout(8000)
   .open('/')
-  .waitForText('Submit')
+  .waitForPageToLoad(5000)
   .clickAndWait('//input[@value="Submit"]')
   .clickAndWait('link=Settings')
   .type('user[name][first]', 'TJ')
